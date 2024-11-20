c6107cb7901e54509f4c24034448f95f7e9baf72
Pontus Gavelin <pontus.gavelin@trr.se>
2024-11-20 18:45:06 +0100

    fix: add piano route

---
+++ src/pages/piano/index.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+const keys = [
+  { note: "C", type: "white" },
+  { note: "C#", type: "black" },
+  { note: "D", type: "white" },
+  { note: "D#", type: "black" },
+  { note: "E", type: "white" },
+  { note: "F", type: "white" },
+  { note: "F#", type: "black" },
+  { note: "G", type: "white" },
+  { note: "G#", type: "black" },
+  { note: "A", type: "white" },
+  { note: "A#", type: "black" },
+  { note: "B", type: "white" },
+]
+
+const Piano = () => {
+  const playNote = (note: string) => {
+    console.log(`Playing note: ${note}`)
+    // Add sound playback logic here
+  }
+
+  return (
+    <div className="piano">
+      {keys.map((key) => (
+        <button
+          key={key.note}
+          className={`key ${key.type}`}
+          onClick={() => playNote(key.note)}
+        >
+          {key.note}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default Piano
